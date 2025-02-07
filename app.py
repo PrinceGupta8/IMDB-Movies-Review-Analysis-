31033f23a2e617e224e6caa99522e2b57036325a
PG <princegupta995643@gmail.com>
2025-02-08 01:04:24 +0530

    commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
+import tensorflow
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.models import Sequential
